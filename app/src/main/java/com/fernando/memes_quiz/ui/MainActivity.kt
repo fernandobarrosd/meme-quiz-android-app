8a53513dc0fcbe386deac6ebded340f25905cb3b
Fernando de Barros <fdebarros0910-2004@hotmail.com>
2023-12-31 13:17:05 -0300

    Init android project

---
+++ app/src/main/java/com/fernando/memes_quiz/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.fernando.memes_quiz.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.fernando.memes_quiz.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
